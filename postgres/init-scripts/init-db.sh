351ae246ba9c5ac099e0f086ac970b492cab25df
lazare-dev <lazare.dec@gmail.com>
2023-09-16 12:20:58 -0700

    updated db config

--- postgres/init-scripts/init-db.sh
+++ postgres/init-scripts/init-db.sh
@@ -14,10 +14,15 @@ else
 EOSQL
 fi
 
-# Create a PostgreSQL database and set the owner
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE $POSTGRES_DB;
-    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+# Check if the PostgreSQL database exists before creating it
+if psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$POSTGRES_DB'" | grep -q 1; then
+    echo "Database $POSTGRES_DB already exists."
+else
+    # Create a PostgreSQL database and set the owner
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
+        CREATE DATABASE $POSTGRES_DB;
+        GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
 EOSQL
+fi
 
 # No need to stop the PostgreSQL service here
