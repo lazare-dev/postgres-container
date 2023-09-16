45be077f0382cee2141f2bc1add4cf79497c2bed
lazare-dev <lazare.dec@gmail.com>
2023-09-16 11:34:45 -0700

    added postgresql.conf and deployment files

--- postgres/init-scripts/init-db.sh
+++ postgres/init-scripts/init-db.sh
@@ -4,10 +4,15 @@ set -e
 # Start the PostgreSQL service
 service postgresql start
 
-# Create a PostgreSQL user
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';
+# Check if the PostgreSQL user exists before creating it
+if psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$POSTGRES_USER'" | grep -q 1; then
+    echo "User $POSTGRES_USER already exists."
+else
+    # Create a PostgreSQL user
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
+        CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';
 EOSQL
+fi
 
 # Create a PostgreSQL database and set the owner
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
@@ -15,5 +20,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
 EOSQL
 
-# Stop the PostgreSQL service
-service postgresql stop
+# No need to stop the PostgreSQL service here
